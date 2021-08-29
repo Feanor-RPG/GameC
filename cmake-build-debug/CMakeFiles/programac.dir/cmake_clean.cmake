a479861e1e3624ec3bb5e7c8df002eb93f0b026b
Feanor-RPG <igorm4aster@gmail.com>
2021-08-29 19:28:27 -0300

    jogoc

---
+++ cmake-build-debug/CMakeFiles/programac.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programac.dir/jogo.c.obj"
+  "libprogramac.dll.a"
+  "programac.exe"
+  "programac.exe.manifest"
+  "programac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/programac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
